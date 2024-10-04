fd16cfe8aa7952324cea73f1eb3b904fb0c4f239
pipysk1 <lvmai1801@gmail.com>
2024-10-04 10:54:53 +0700

    all

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-audio-mailv/", // YOUR REPO NAME HERE
-});
+})
