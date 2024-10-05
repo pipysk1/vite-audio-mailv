ee763175c0abc15786be3baf78178a6b40fb41cf
pipysk1 <lvmai1801@gmail.com>
2024-10-05 18:19:54 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import Siderbar from './components/Siderbar'
-import Player from './components/Player'
-import Display from './components/Display'
+
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
-      <div className='h-[95%] flex'>
-        <Siderbar />
-        <Display />
-      </div>
-      <Player />
-    </div>
+    <>
+    Hello
+    </>
   )
 }
 
